3d78ded318af8950b677abfcc466654db7fdde0b
Sami Gakhar <117925735+samigakhar20@users.noreply.github.com>
2026-04-16 12:52:44 +0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,14 +1,24 @@
-// Firebase Configuration (Updated from Screenshot 61)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDD0AuOKrxIuLI_ZPzx8rCVa_GAjgx3b3bc",
+  apiKey: "AIzaSyDDAu0KrxIulI_ZPzx8rCVa_GAJgx3b3bc",
   authDomain: "dropify-caa0c.firebaseapp.com",
   projectId: "dropify-caa0c",
   storageBucket: "dropify-caa0c.firebasestorage.app",
-  messagingSenderId: "1233868442768",
-  appId: "1:1233868442768:web:2478356a8566ed499b3449",
+  messagingSenderId: "123386844268",
+  appId: "1:123386844268:web:2478356a8566ed499b3449",
   measurementId: "G-HET1M48B2E"
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 // Initialize Firebase (Using Compat Version for your HTML setup)
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
