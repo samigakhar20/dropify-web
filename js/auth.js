05cd32e3726201c2423929b2d4c6458e19cd106e
Sami Gakhar <117925735+samigakhar20@users.noreply.github.com>
2026-04-16 13:11:42 +0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -11,9 +11,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const auth = firebase.auth();
